a3277dc193ad63547ed76d7b9b2572734aad9669
My PC <amolkupade570@gmail.com>
2023-10-29 09:25:30 +0530

    'pro'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { BannerComponent } from './banner/banner.component';
+import { FacilityComponent } from './facility/facility.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SidenavComponent,
+    BannerComponent,
+    FacilityComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
